efbbc97f877a650e1a157950680a9a73c864f8af
VladGan <vladgan2210@ukr.net>
2014-05-12 19:40:11 +0300

    ыфв

--- js/code3.js
+++ js/code3.js
@@ -1,3 +1,38 @@
+/*function firstSlide(){
+	nonBolt();
+	document.querySelector('.menu_nav_ul_first').style.cssText="font-weight: bold;";
+	document.querySelector('.menu_arrow').style.cssText=" top: 143px; transition: 1s";
+}
+function secondSlide(){
+	nonBolt();
+	document.querySelector('.menu_nav_ul_second').style.cssText="font-weight: bold;";
+	document.querySelector('.menu_arrow').style.cssText=" top: 191px; transition: 1s";
+}
+function thirdSlide(){
+	nonBolt();
+	document.querySelector('.menu_nav_ul_third').style.cssText="font-weight: bold;";
+	document.querySelector('.menu_arrow').style.cssText=" top: 239px; transition: 1s";
+}
+function fourthSlide(){
+	nonBolt();
+	document.querySelector('.menu_nav_ul_fourth').style.cssText="font-weight: bold;";
+	document.querySelector('.menu_arrow').style.cssText=" top: 286px; transition: 1s";
+}
+function fifthSlide(){
+	nonBolt();
+	document.querySelector('.menu_nav_ul_fifth').style.cssText="font-weight: bold;";
+	document.querySelector('.menu_arrow').style.cssText=" top: 335px; transition: 1s";
+}
+function nonBolt(){
+	document.querySelector('.menu_nav_ul_first').style.cssText="font-weight: normal;";
+	document.querySelector('.menu_nav_ul_second').style.cssText="font-weight: normal;";
+	document.querySelector('.menu_nav_ul_third').style.cssText="font-weight: normal;";
+	document.querySelector('.menu_nav_ul_fourth').style.cssText="font-weight: normal;";
+	document.querySelector('.menu_nav_ul_fifth').style.cssText="font-weight: normal;";
+}
+*/
+/* Funktionalität von den Slider Block*/
+
 $(document).ready(function(){
 
 
@@ -22,6 +57,15 @@ $(document).ready(function(){
 			});
 		},
 
+	moveSlidePosition: function(event)
+	{
+	// Magic Numbers
+		this.el.allSlides.css(
+		{
+			"background-position": $(event.target).scrollLeft()/6-100+ "px 0"
+		});
+	},
+
 	handleNavClick: function(event, el) 
 	{
 
@@ -39,9 +83,43 @@ $(document).ready(function(){
 		$(".slider-block__photo__img").animate({
 			'left': $left
 		},this.timing);
-	}
+	},
+
+	changeActiveNav: function(el)
+		{
+// Remove active from all links
+			this.el.allNavButtons.removeClass("active");
+// Add back to the one that was pressed
+			$(el).addClass("active");
+		}
 	};
 
 slider.init();
 });
 
+
+
+
+
+
+
+/*$('#1SliderBlock_Buttons_Round').css("background-color","white");
+
+	CurrentId = 1;
+
+	setInterval(function()
+		{
+			s = "SliderBlock_Buttons_Round";
+			s = CurrentId + s;
+			$("#" + s).css("background-color","transparent");
+
+			s = "SliderBlock_Buttons_Round";
+			if (CurrentId == 5) CurrentId = 0;
+			s = (CurrentId+1) + s;
+			$("#slider-block__photo__img").attr("src","img/Slide1.png");
+			$("#" + s).css("background-color","white");
+
+			CurrentId++;
+			CurrentId%=6;
+			if (CurrentId == 0) CurrentId++;
+		},10000);*/
