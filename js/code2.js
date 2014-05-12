75fcbf1d706463a546eb9b19e55394b97ae5815d
VladGan <vladgan2210@ukr.net>
2014-05-12 22:52:57 +0300

    Слайдер и плюсы

--- js/code2.js
+++ js/code2.js
@@ -36,11 +36,22 @@ function nonBolt(){
 $(document).ready(function(){
 
 	$('.info-block__plus-info').css( "display", "none");
+	$('#1PlusInfo').css( "display", "block");
 
 	$('.info-block__plus').click( function(event)
 	{
 		id = event.target.id;
 		s = id[0] + "PlusInfo";
+
+		var img = new Image();
+		img.onload = function() 
+		{
+			$(".img-wrapper").css("height",this.height + "px!important");
+			$(".info-block__pluses__img").attr("src","img/"+id[0]+".png");
+			$(".info-block__pluses__img").css("left",500 - this.height/2+"px");
+		}
+
+		img.src = "img/" + id[0] + ".png";
 
 		if ($('#' + s).css( "display" ) == "block")
 			$('#' + s).css( "display", "none");
