6725e5eaa70747994a5985c37160622f1fc0d229
VladGan <vladgan2210@ukr.net>
2014-05-15 17:20:57 +0300

    Update code2.js

--- js/code2.js
+++ js/code2.js
@@ -1,36 +1,3 @@
-/*function firstSlide(){
-	nonBolt();
-	document.querySelector('.menu_nav_ul_first').style.cssText="font-weight: bold;";
-	document.querySelector('.menu_arrow').style.cssText=" top: 143px; transition: 1s";
-}
-function secondSlide(){
-	nonBolt();
-	document.querySelector('.menu_nav_ul_second').style.cssText="font-weight: bold;";
-	document.querySelector('.menu_arrow').style.cssText=" top: 191px; transition: 1s";
-}
-function thirdSlide(){
-	nonBolt();
-	document.querySelector('.menu_nav_ul_third').style.cssText="font-weight: bold;";
-	document.querySelector('.menu_arrow').style.cssText=" top: 239px; transition: 1s";
-}
-function fourthSlide(){
-	nonBolt();
-	document.querySelector('.menu_nav_ul_fourth').style.cssText="font-weight: bold;";
-	document.querySelector('.menu_arrow').style.cssText=" top: 286px; transition: 1s";
-}
-function fifthSlide(){
-	nonBolt();
-	document.querySelector('.menu_nav_ul_fifth').style.cssText="font-weight: bold;";
-	document.querySelector('.menu_arrow').style.cssText=" top: 335px; transition: 1s";
-}
-function nonBolt(){
-	document.querySelector('.menu_nav_ul_first').style.cssText="font-weight: normal;";
-	document.querySelector('.menu_nav_ul_second').style.cssText="font-weight: normal;";
-	document.querySelector('.menu_nav_ul_third').style.cssText="font-weight: normal;";
-	document.querySelector('.menu_nav_ul_fourth').style.cssText="font-weight: normal;";
-	document.querySelector('.menu_nav_ul_fifth').style.cssText="font-weight: normal;";
-}
-*/
 /* Funktionalität von den Plus Block*/
 
 $(document).ready(function(){
