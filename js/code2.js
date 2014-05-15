4925bee5bfa9545bb27bff9a1238cd3be55ff225
VladGan <vladgan2210@ukr.net>
2014-05-15 18:14:03 +0300

    Update code2.js

--- js/code2.js
+++ js/code2.js
@@ -28,5 +28,49 @@ $(document).ready(function(){
 			$('#' + s).css( "display","block");
 		}
 	});
+/* Funktionalität von den Slider Block*/
 
+
+
+	var slider = 
+	{
+		el:
+		{
+			slider: $(".slider-block__photo"),
+			allSlides: $(".slider-block__photo__img"),
+			sliderNav: $(".slider-block__buttons__round"),
+			allNavButtons: $(".slider-block__buttons__round")
+		},
+
+		timing: 800,
+
+		init: function()
+		{
+			this.el.sliderNav.on("click",function(event) 
+			{
+				slider.handleNavClick(event, this);
+			});
+		},
+
+	handleNavClick: function(event, el) 
+	{
+
+		event.preventDefault();
+
+		var Id = el.id;
+		$(".slider-block__buttons__round").css("background-color","transparent");
+		$("#" + Id).css("background-color","white");
+		Id = el.id [0];
+
+
+		var $left = -(Id-1)*664;
+
+		$left = $left.toString() + "px";
+		$(".slider-block__photo__img").animate({
+			'left': $left
+		},this.timing);
+	}
+	};
+
+slider.init();
 });
